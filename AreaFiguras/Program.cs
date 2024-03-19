4fcfd4ed3abb52feddb8d1c3ff107aa896053bea
Jailton|Junior14 <jailtonjrbr@gmail.com>
2024-03-19 11:19:09 -0300

    Adicionar arquivos de projeto.

---
+++ AreaFiguras/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaFiguras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figura;
+            double bas, alt, basm, basM;
+
+            Console.WriteLine("Escolha uma figura: Quadrado(Q), Retângulo(R), Trapézio(T), Losango(L)");
+            figura = Console.ReadLine().ToLower();
+
+            switch (figura)
+            {
+                case "q":
+                    Console.WriteLine("Digite a altura: ");
+                    bas = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite a base: ");
+                    alt = double.Parse(Console.ReadLine());
+                    Console.WriteLine("A area do Quadrado é: "+ (bas*alt));
+                    break;
+                case "r":
+                    Console.WriteLine("Digite a altura: ");
+                    bas = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite a base: ");
+                    alt = double.Parse(Console.ReadLine());
+                    Console.WriteLine("A area do Retângulo é: " + (bas * alt));
+                    break;
+                case "t":
+                    Console.WriteLine("Digite a altura: ");
+                    alt = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite a base menor: ");
+                    basm = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite a base maior: ");
+                    basM = double.Parse(Console.ReadLine());
+                    Console.WriteLine("A area do Trapézio é: " + ((basm + basM)*alt)/2);
+                    break;
+                case "l":
+                    Console.WriteLine("Digite a diagonal menor: ");
+                    basm = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite a diagonal maior: ");
+                    basM = double.Parse(Console.ReadLine());
+                    Console.WriteLine("A area do Trapézio é: " + (basm * basM) / 2);
+                    break;
+                default: Console.WriteLine("Opição invalida");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
